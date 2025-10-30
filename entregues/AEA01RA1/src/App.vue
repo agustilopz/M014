faf2ca1add44be48a40e4f070ac87b5d34553878
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-10-30 17:43:11 +0100

    feat: add aea01ra01

---
+++ entregues/AEA01RA1/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const nomUsuari = ref('')
+</script>
+
+<template>
+  <h1>Gestor de Tasques</h1>
+
+<div>
+<!--input-->
+<p>Nom de l'usuari: {{ nomUsuari }}</p>
+<input v-model="nomUsuari" placeholder="Escriu una nova tasca" ></input>
+<button>Afegir</button><br>
+<input type="checkbox" /> Mostrar només tasques pendents
+
+</div>
+
+
+</template>
+
+<style scoped></style>
+
+
+
