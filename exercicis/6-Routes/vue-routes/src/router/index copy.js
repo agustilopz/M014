96df2a2bc14dfc1cd17e1f0b5b15dc985c3ff5b1
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-11-18 18:40:28 +0100

    afegir lazy routes

--- exercicis/6-Routes/vue-routes/src/router/index copy.js
+++ exercicis/6-Routes/vue-routes/src/router/index copy.js
@@ -1,31 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-/*
 import Home from '@/views/Home.vue'
 import Frameworks from '@/views/Frameworks.vue'
 import FrameworkDetail from '@/views/FrameworkDetail.vue'
 import FrameworkDetail2 from '@/views/FrameworkDetail2.vue'
 import FrameworkVersion from '../views/FrameworkVersion.vue'
-*/
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
 
     {
       path: '/',
-      name: 'home',     
-        component: () => import('@/views/Home.vue') 
+      name: 'home',
+      component: Home
+      /*Lazy route (nomes importar el component quan es visita la ruta) 
+        component: () => import('../views/Home.vue') */
     },
     {
       path: '/frameworks',
       name: 'frameworks',
-      component: () => import('@/views/Frameworks.vue'),
+      component: Frameworks,
       children: [
         {
           path: ':id',
           name: 'frameworkDetail',
-          component: () => import('@/views/FrameworkDetail2.vue'),
+          component: FrameworkDetail2,
         },
       ]
     },
