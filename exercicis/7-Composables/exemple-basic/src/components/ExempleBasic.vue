93b7f546731d2b74355154b21ee07976bd34ccbb
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-12-02 17:36:42 +0100

    afegir exemple basic composable

---
+++ exercicis/7-Composables/exemple-basic/src/components/ExempleBasic.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useCounter } from '../composables/useCounter.js';
+
+const { count, increment, decrement } = useCounter();
+
+</script>
+
+<template>
+
+<h1>Prova Composable</h1>
+
+<div class="card">
+<p>Count: {{ count }}</p>
+<button @click="increment">+</button>
+<button @click="decrement">-</button>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
