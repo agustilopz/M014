d264123db61dc323fa9e8f46180190d30d19328e
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-12-02 18:53:10 +0100

    update useFetch

--- exercicis/7-Composables/exemple-basic/src/composables/useFetch.js
+++ exercicis/7-Composables/exemple-basic/src/composables/useFetch.js
@@ -7,13 +7,15 @@ export function useFetch(url) {
     const loading = ref(false);
 
     const fetchData = async () => {
+        loading.value = true;
+        error.value = null;
         try {
             const res = await fetch(url);
             if (!res.ok) throw new Error('Error al fetch: ' + res.status);
             data.value =  await res.json();
 
         } catch (err) {
-            error.value = err;
+            error.value = err.message;
 
         } finally {
             loading.value = false;
