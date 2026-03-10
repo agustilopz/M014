825462fad71989582c91642a70e44c9dfa5146cf
agusti.lopez <agusti.lopez.7e8@itb.cat>
2026-03-10 19:10:29 +0100

    feat: afegir primer exercici quasar

---
+++ exercicis/RA4/quasar-project/src/pages/LlistatPage.vue
@@ -0,0 +1,14 @@
+<template>
+  listId: {{ listId }}
+    <ul>
+      <li>a</li>
+      <li>b</li>
+      <li>c</li>
+    </ul>
+</template>
+
+<script setup>
+defineProps({
+  listId: String
+})
+</script>
